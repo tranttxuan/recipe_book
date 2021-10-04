b24fb43b67fda51f89f78593b9e7af81b6f71d91
xuan.tranthithanh <xuan.tranthithanh@thalesdigital.io>
2021-10-04 23:46:19 +0200

    feat(repo): add routes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,9 +6,6 @@ import { Component } from '@angular/core';
     styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-    loadedFeature = 'recipe'
+
     title = 'recipe-app';
-    onNavigate(feature: string) {
-        this.loadedFeature = feature;
-    }
 }
--- src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -1,4 +1,5 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params, Router } from '@angular/router';
 import { Recipe } from '../recipe.model';
 import { RecipeService } from '../recipe.service';
 
@@ -8,13 +9,26 @@ import { RecipeService } from '../recipe.service';
     styleUrls: ['./recipe-detail.component.css']
 })
 export class RecipeDetailComponent implements OnInit {
-    @Input() recipe: Recipe;
-    constructor(private recipeService: RecipeService) { 
+  recipe: Recipe;
+    constructor(private recipeService: RecipeService,
+        private route: ActivatedRoute,
+        private router: Router) {
     }
 
-    ngOnInit(): void {
+    ngOnInit() {
+        // const idParam = +this.route.snapshot.params["id"]
+        // this.recipe = this.recipeService.getRecipe(idParam)
+        this.route.params.subscribe(
+            (params:Params)=>{
+                this.recipe = this.recipeService.getRecipe(+params["id"])
+            }
+        )
+
     }
     onAddToShoppingList() {
         this.recipeService.addIngredientToShoppingList(this.recipe.ingredients)
+    }
+    onEditRecipe(){
+this.router.navigate(['edit'], {relativeTo:this.route})
     }
 }
