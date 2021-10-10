45a0d7e33fdf2902f3581ba2244aca65db365ce9
xuan.tranthithanh <xuan.tranthithanh@thalesdigital.io>
2021-10-10 08:28:48 +0200

    fix(shopping-list): use Subject for ingredientChanged

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
 import { Ingredient } from '../shared/ingredient.model';
 import { ShoppingListService } from './shopping-list.service';
 
@@ -8,20 +9,23 @@ import { ShoppingListService } from './shopping-list.service';
     styleUrls: ['./shopping-list.component.css'],
 })
 
-export class ShoppingListComponent implements OnInit {
+export class ShoppingListComponent implements OnInit, OnDestroy {
     ingredients: Ingredient[] = [];
+    private igChangeSub: Subscription;
 
     constructor(private shoppingListService: ShoppingListService) { }
 
     ngOnInit(): void {
         this.ingredients = this.shoppingListService.getIngredients();
-        this.shoppingListService.ingredientsChanged.subscribe(
-            (ingredients:Ingredient[])=>{
-                this.ingredients = ingredients;
-                console.log('all ingredients-------: ', this.ingredients)
-            }
-        )
-      
+        this.igChangeSub = this.shoppingListService.ingredientsChanged
+            .subscribe(
+                (ingredients: Ingredient[]) => {
+                    this.ingredients = ingredients;
+                }
+            )
+    }
+    ngOnDestroy(){
+        this.igChangeSub.unsubscribe();
     }
 
 }
